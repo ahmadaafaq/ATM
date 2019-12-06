9dedc6b5f06bcbb17a191aefc0b91fcb695f0f90
Aafaq Ahmad <aafaq.ahmad@rsystems.com>
2019-12-06 16:36:05 +0530

    Updates

---
+++ precache-manifest.bf054ff96dde89f700c12761da783e11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32259b1ec52c426b9b86a816194a8045",
+    "url": "/ATM/index.html"
+  },
+  {
+    "revision": "f21c93ce19bd6cecec9b",
+    "url": "/ATM/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0e14b52258e7e6a859d6",
+    "url": "/ATM/static/js/2.9f078011.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ATM/static/js/2.9f078011.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f21c93ce19bd6cecec9b",
+    "url": "/ATM/static/js/main.f5c170c4.chunk.js"
+  },
+  {
+    "revision": "7b23a1e02d48e78725ac",
+    "url": "/ATM/static/js/runtime-main.cd905899.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ATM/static/media/logo.5d5d9eef.svg"
+  }
+]);
